9aa9b7c3d67dc9feed68d869b0799b0aebb526f7
milo <tranmilo66@gmail.com>
2020-07-14 09:58:52 +0700

    change files in 14/07

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
+
 
 serviceWorker.unregister();

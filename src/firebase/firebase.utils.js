749768266101d83fce137e6a07b127c2e6e1446b
milo <tranmilo66@gmail.com>
2020-09-02 08:34:02 +0700

    pull collections data from firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -52,6 +52,21 @@ export const addCollectionAndDocument = (collectionKey, objectToAdd) => {
   batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  console.log(transformedCollection);
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
